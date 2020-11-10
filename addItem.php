8c0511bd8fc7598ee782cb60f3d1736237a72940
justushache <33901877+justushache@users.noreply.github.com>
2020-11-10 15:29:18 +0100

    Add files via upload

--- addItem.php
+++ addItem.php
@@ -17,7 +17,9 @@
 </ul>
 
 
-<form method="post">
+<form action="#" method="post" enctype="multipart/form-data">
+	<input type="file" name="fileToUpload" id="fileToUpload">
+	<input type="submit" value="Upload">
   <div class="form-group">
     <label for="name">Name</label>
     <input type="text" class="form-control" name="name">
@@ -34,16 +36,40 @@
     <label for="description">Description</label>
     <input type="text" class="form-control" name="description">
   </div>
+  <div class="form-group">
+    <label for="homepage">Homepage.php</label>
+    <input type="text" class="form-control" name="homepage">
+  </div>
   <button type="submit" class="btn btn-primary">Add a new item</button>
 </form>
 
 <?php
-  if(isset($_POST["name"])&&isset($_POST["manufacturer"])&&isset($_POST["number"])&&isset($_POST["description"])){
+  if(isset($_POST["name"])&&isset($_POST["manufacturer"])&&isset($_POST["number"])&&isset($_POST["description"])&&isset($_POST["homepage"])){
     echo ($_POST["name"]);
+	//save img as millis+productname.dateiextension
+
+	$target_dir = "uploads";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	$imagepath = round(microtime(true)*1000).$_POST["name"].".".$imageFileType;
+	if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'],"$target_dir/".$imagepath)==false){
+		echo "we had an error";
+	}
+	
+	//save image name
     $pdo = new PDO('mysql:host=localhost;dbname=signin', 'root', '');
-    $sql = 'INSERT INTO products (name, manufacturer,number,description) VALUES ("'.$_POST["name"].'","'.$_POST["manufacturer"].'","'.$_POST["number"].'","'.$_POST["description"].'")';
+    $sql = 'INSERT INTO products (name, manufacturer,number,description,imagepath,homepage) VALUES ("'
+	.$_POST["name"].'","'
+	.$_POST["manufacturer"].'","'
+	.$_POST["number"].'","'
+	.$_POST["description"].'","'
+	.$imagepath.'","'
+	.$_POST["homepage"].'")';
     $pdo->query($sql);
     echo ($sql);
+  }else{
+	  echo("not all field are populated");
   }
 ?>
 
