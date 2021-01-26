28de7826377c68e4ebd13d6bea3f2cb52fb0b0eb
justushache <33901877+justushache@users.noreply.github.com>
2021-01-26 15:24:33 +0100

    add brackets to forbidden charakters

--- validateInputText.php
+++ validateInputText.php
@@ -1,6 +1,6 @@
 <?php
     function removeCriticalText($text){
-        $toReplace = array(";", "<", ">", "/", "\\", "*", "\"", "'", "=");
+        $toReplace = array(";", "<", ">", "/", "\\", "*", "\"", "'", "=","{","}");
         //removes given characters or text passages to prevent sql injections ore executable html code
         foreach($toReplace as $replace){
             $text = str_replace($replace, "", $text);
